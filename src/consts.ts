f3b67a6b9d6c635e1aae2418e6174dba2aecb0af
Fausto Moya <faustomoya99@gmail.com>
2024-10-23 20:17:10 +0200

    fix: dates

--- src/consts.ts
+++ src/consts.ts
@@ -41,7 +41,7 @@ export const WORK_EXPERIENCE: WorkExperience = [
     role: "Technical Lead",
     technologies: ["Kotlin", "Spring", "MongoDB", "Kafka"],
     description: "At Albert Heijn, I led the modernization of the workforce management tool, building a new integration using modern languages and techniques. I spearheaded efforts to improve code quality, promoting good coding practices and a test-driven development mindset.\nI played a key role in helping the team adopt agile methodologies, improving meeting efficiency and sharing agile knowledge to streamline processes. By fostering close collaboration between the business and development teams, I enabled better requirements gathering and greater ownership of projects.",
-    start: new Date('2022-08-01'),
+    start: new Date('2024-08-01'),
     end: null,
     link: ""
   },
@@ -51,7 +51,7 @@ export const WORK_EXPERIENCE: WorkExperience = [
     technologies: ["Kotlin", "Spring", "Postgres", "GraphQL", "ActiveMQ", "AWS"],
     description: `I led the design and implementation of a Trade Lane reporting and optimization tool, significantly enhancing data analysis and reporting capabilities. I optimized API response times, achieving a uniform 200 milliseconds for all data sets, compared to previous times of 2 seconds for small sets and 10 seconds for large sets.\nI successfully migrated 29 services to AWS, establishing infrastructure as code and implementing new CI/CD pipelines that resulted in a 30% reduction in operational costs. Additionally, I reduced deployment times by 25% through enhancements to the testing suite, GitHub Actions pipeline, and AWS Fargate integration, ensuring faster and more efficient software delivery.`,
     start: new Date('2022-05-01'),
-    end: new Date('2024-07-31'),
+    end: new Date('2024-08-01'),
     link: ""
   },
   {
@@ -60,7 +60,7 @@ export const WORK_EXPERIENCE: WorkExperience = [
     technologies: ["Java", "Kotlin", "SparkJava", "Lucene"],
     description: "As a Software Engineer at Mercado Libre, I led the development of innovative solutions that played a key role in the product's success. I designed and implemented scalable, robust, and maintainable systems that exceeded customer expectations, directly contributing to the company's growth. By utilizing modern technologies and agile methodologies, I consistently delivered high-quality results.\nThrough collaboration with cross-functional teams, we established and enforced best practices, striving for excellence in performance and continuous improvement. Notably, I improved store refresh speed by 50%, resulting in faster store indexing and a better customer experience.",
     start: new Date('2021-05-01'),
-    end: new Date('2022-04-30'),
+    end: new Date('2022-05-01'),
     link: "",
   },
   {
@@ -68,8 +68,8 @@ export const WORK_EXPERIENCE: WorkExperience = [
     role: "Software Engineer",
     technologies: ["Java", "AWS Lambda", "AWS SQS", "AWS SNS", "AWS Api Gateway"],
     description: "As a Java Software Engineer at Ualá, I contributed to the development of robust and scalable software solutions that adhered to the highest industry standards. I leveraged modern technologies, including a full serverless infrastructure in AWS, and worked closely with my team using agile methodologies to deliver high-quality projects.\nA key part of my role was defining development standards and best practices to optimize the team's performance. I wrote clean and efficient code to ensure the optimal functioning of our solutions, which directly impacted Ualá's success. Throughout my experience, I deepened my knowledge of cutting-edge technologies and gained valuable insights into the software development process, consistently delivering results that exceeded expectations.",
-    start: new Date('2021-05-01'),
-    end: new Date('2020-08-31'),
+    start: new Date('2020-09-01'),
+    end: new Date('2021-05-01'),
     link: ""
   },
   {
@@ -77,8 +77,8 @@ export const WORK_EXPERIENCE: WorkExperience = [
     role: "Software Engineer",
     technologies: ["Kotlin", "Spring", "Postgres", "ActiveMQ", "RxJava"],
     description: "As a Kotlin Software Engineer at Rappi, I was part of a high-performing team that delivered innovative and efficient software solutions. My role involved developing new features, improving code, and fixing bugs, while collaborating closely with Scrum Masters and Project Managers to keep projects on track. I played a key role in integrating the app with various service providers, enabling offerings like Home Services, Video Conference Doctors, Games, and Sports Betting.\nIn addition to my development work, I provided technical guidance to my colleagues and mentored new team members. My contributions helped the team exceed expectations and positively impacted the development of scalable and robust software solutions for Rappi.",
-    start: new Date('2020-09-01'),
-    end: new Date('2019-07-31'),
+    start: new Date('2019-08-01'),
+    end: new Date('2020-09-01'),
     link: ""
   }
 ]
