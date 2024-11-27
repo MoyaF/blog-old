afa16a206751349f9d32891d3a8fb1b46b1cd16d
Fausto Moya <37676075+MoyaF@users.noreply.github.com>
2024-11-27 23:35:03 +0100

    feature: add end 2 end tests (#69)
    * feature: add github actions e2e test
    
    * feature: add Playwright testing setup and initial tests
    
    * feature: enhance e2e test workflow with separate browser installation and report upload
    
    * feature: comment out unused browser configurations in Playwright setup
    
    * feature: update home page tests to use labels and add email link test
    
    * feature: simplify test descriptions for home page tests
    
    * feature: remove unnecessary await from link retrieval in home page tests
    
    * feature: add tests for blog and projects links on home page
    
    * feature: refactor home page tests for consistency and readability
    
    * feature: add navigation tests for blog, projects, and experience links
    
    * feature: add tests for experience section visibility
    
    * feature: add test for visibility of Projects heading
    
    * feature: add test for visibility of Blog heading
    
    * feature: add playwright cache
    
    ---------
    
    Co-authored-by: Fausto Moya <faustomoya99@gmail.com>

---
+++ test/blog.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "@playwright/test";
+
+test("should display the Blog heading", async ({ page }) => {
+  await page.goto("/blog");
+  await expect(
+    page.getByRole("heading", { name: "Blog" }),
+  ).toBeVisible();
+});
---
+++ test/projects.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "@playwright/test";
+
+test("should display the Projects heading", async ({ page }) => {
+  await page.goto("/projects");
+  await expect(
+    page.getByRole("heading", { name: "Projects" }),
+  ).toBeVisible();
+});
